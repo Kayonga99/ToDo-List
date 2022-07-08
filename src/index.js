29bcd29d17d20334a0c23c2795d221c30be3e8bb
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-07-08 18:51:04 +0200

    Add objects.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 const taskList = [
-  
+  {
+    description: 'Shopping',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Study ES6 - Webpack',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Complete Awesome Books ES6',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Workout',
+    completed: true,
+    index: 3,
+  },
 ]
