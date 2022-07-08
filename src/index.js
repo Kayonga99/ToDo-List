0d48031fc0aba91a75e50f010239343a93fa4253
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-07-08 18:55:56 +0200

    Update index.js.

--- src/index.js
+++ src/index.js
@@ -22,3 +22,27 @@ const taskList = [
 ]
 
 const mainContainer = document.querySelector(".todo-list-container")
+mainContainer.innerHTML = `<div class="row">
+<h1>Today's To Do</h1>
+<i class="fa-solid fa-rotate fa-lg font-awesome-icon"></i>
+</div>
+<div>
+<input placeholder="Add to your list...">
+</div>`;
+
+taskList.forEach((e) => {
+  let isChecked;
+  let strikeThrough;
+  if (e.completed === true) {
+    isChecked = 'checked';
+    strikeThrough = 'strike-through';
+  }
+
+  mainContainer.innerHTML += `<div class="row">
+  <input class="checkbox" type="checkbox" ${isChecked}>
+  <p class="${strikeThrough}">${e.description}</p>
+  <i class="fa-solid fa-ellipsis-vertical fa-lg font-awesome-icon"></i>
+  </div>`;
+});
+
+mainContainer.innerHTML += '<button class="button">Clear all completed</button>';
