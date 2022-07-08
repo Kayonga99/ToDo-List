4f45205add9362b447320877861d52f1a43b42b8
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-07-08 22:53:58 +0200

    Import js files, remove tasklist array.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
 import './index.css';
+import addElem from './modules/addElement';
+import TaskList from './modules/classTaskList';
+import refreshList from './modules/refresh';
+
+const taskList = new TaskList();
 
 const mainContainer = document.querySelector('.todo-list-container');
-
-const taskList = [
-  {
-    description: 'Shopping',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Study ES6 - Webpack',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Complete Awesome Books ES6',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Workout',
-    completed: true,
-    index: 3,
-  },
-];
 
 mainContainer.innerHTML = `<div class="row">
 <h1>Today's To Do</h1>
